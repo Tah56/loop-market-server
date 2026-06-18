5a1fce5a0cd93c54170c09053b5d4f0d018b72a7
Tah56 <tanzimahmed575@gmail.com>
2026-06-19 01:23:10 +0600

    search product with product id

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 
+
+
+
+
 const uri = process.env.DB_URI
 
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,13 +33,50 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
+    
+
     await client.connect();
+
+
+    const database =client.db('loopMarket')
+    const productsCollection = database.collection('products')
+
+
+app.post("/api/products",async(req,res)=>{
+const products = req.body;
+const result = await productsCollection.insertOne(products)
+res.send(result)
+})
+
+
+app.get("/api/products",async(req,res)=>{
+const result = await productsCollection.find().toArray()
+res.send(result)
+})
+
+
+app.get("/api/products/:id",async(req,res)=>{
+  const {id} = req.params
+const result = await productsCollection.findOne({ _id: new ObjectId(id) } )
+res.send(result)
+})
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   
   }
 }
 run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
